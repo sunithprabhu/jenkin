11c834ff43e7b32fd2a2b7121a0a43b085a2864a
sunithprabhu <sunithprabhu@gmail.com>
2020-05-13 19:49:49 +0530

    Sending to jenkins Marry_Maa project

---
+++ Thread Collections/src/Jesus.java
@@ -0,0 +1,22 @@
+
+public class Jesus {
+
+	int id ;
+	
+	String name;
+	
+	
+	public Jesus(int id,String name)
+	{
+		this.id = id;
+		
+		this.name = name;
+	}
+	
+	
+	public String toString()
+	{
+		return "'id' + id + name + i";
+	}
+	
+}
